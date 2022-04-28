5a8711b0d8846f9ad937a1acbc755c6321a9827f
Anton Sukharev <sweiba@gmail.com>
2022-04-28 15:00:45 +0300

    added color property

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -40,17 +40,17 @@ describe("Pact Verification", () => {
     const stateHandlers = {
       "products exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "yellow")]
         ]);
       },
       "products exist": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red")]
         ]);
       },
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "green")]
         ]);
       },
       "a product with ID 11 does not exist": () => {
